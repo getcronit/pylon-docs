bb04d0a759956ec0e2c81c1c57501662c2287a68
Nico Schett <52858351+schettn@users.noreply.github.com>
2024-05-21 15:54:06 +0000

    feat: Update DocsStepper to include current page in child pages
    This is required because some pages are not in the docsIndex

--- src/gatsby-plugin-jaen/components/Layout.tsx
+++ src/gatsby-plugin-jaen/components/Layout.tsx
@@ -283,7 +283,7 @@ const Layout: React.FC<LayoutProps> = ({ children, pageProps }) => {
 
             {children}
           </article>
-          <DocsStepper pages={docsIndex.childPages} currentPage={page} />
+          <DocsStepper pages={[page, ...docsIndex.childPages]} currentPage={page} />
         </div>
         <div className="hidden xl:sticky xl:top-[4.5rem] xl:-mr-6 xl:block xl:h-[calc(100vh-4.5rem)] xl:flex-none xl:overflow-y-auto xl:py-16 xl:pr-6">
           <nav aria-labelledby="on-this-page-title" className="w-56">
